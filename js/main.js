20c83e2575ce5b676d3081c0dcfaf36b89b62b3a
Dominic Muttel <dominic.muttel@gmail.com>
2015-10-14 13:28:53 -0600

    corrected errors and added button script -- Time to fuck it all up now that its working -- Making it mine...

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function ($){
 
   //initialise Stellar.js
+  $(window).stellar();
+
+  //Cache some variables
   var links = $('.navigation').find('li');
   slide = $('.slide');
   button = $('.button');
@@ -12,7 +15,7 @@ jQuery(document).ready(function ($){
     dataslide = $(this).attr('data-slide');
     //If the user scrolls up change the navigation liink that has the same data-slide attribute as the slide to active and
     //remove the active class from the previous navigation link
-    if (dirrection === 'down') {
+    if (direction === 'down') {
       $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
     }
     // else if the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and
@@ -25,9 +28,9 @@ jQuery(document).ready(function ($){
   //waypoints doesnt detect the first slide when user scrolls back up to the top so we add thsi little bit of code, that removes the class
   //from navigation link slide 2 and adds it to navigation link slide 1.
   mywindow.scroll(function () {
-    if (mywindow.scrollTop() == 0) {
+    if (mywindow.scrollTop() === 0) {
       $('.navigation li[data-slide="1"]').addClass('active');
-      $('.navigation li[data-slide"2"]').removeClass('active');
+      $('.navigation li[data-slide="2"]').removeClass('active');
     }
   });
   //Create a function that will be passed a slide number and then will scroll to that slide using jQuerys animate. The Jquery
@@ -37,6 +40,14 @@ jQuery(document).ready(function ($){
       scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
     }, 2000, 'easeInOutQuint');
   }
+
+  links.click(function (e){
+    e.preventDefault();
+    dataslide = $(this).attr('data-slide');
+    goToByScroll(dataslide);
+  });
+
+
   //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
   button.click(function (e){
     e.preventDefault();
